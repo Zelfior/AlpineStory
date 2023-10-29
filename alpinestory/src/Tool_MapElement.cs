eed60adf9328a3ad2edb3a8a991358f9fe850818
Zelfior <thibault.moulignier@hotmail.fr>
2023-10-29 21:00:07 +0100

    modif map element

--- alpinestory/src/Tool_MapElement.cs
+++ alpinestory/src/Tool_MapElement.cs
@@ -4,25 +4,29 @@ using Vintagestory.API.Server;
 public class MapElement
 {
     internal ICoreServerAPI api;
-    internal int centerX, centerZ, halfSize;
+    internal int centerX, centerZ, halfSize, scale;
     internal float rotation, pixelWidth;
     internal int[] mapData;
     internal UtilTool uTool;
     internal SKBitmap imageData;
-    public MapElement(ICoreServerAPI api, UtilTool uTool, int centerX, int centerZ, float rotation, int[] mapData, int size, float pixelWidth, SKBitmap imageData){
+    public MapElement(ICoreServerAPI api, UtilTool uTool, int centerX, int centerZ, float rotation, int[] mapData, int size, float pixelWidth, int scale, SKBitmap imageData){
         this.api = api;
         this.uTool = uTool;
         this.centerX = centerX;
         this.centerZ = centerZ;
         this.rotation = rotation;
         this.mapData = mapData;
-        halfSize = size;
+        this.scale = scale;
+
+        halfSize = (int)((float)(size)/2);
+        
         this.pixelWidth = pixelWidth;
         this.imageData = imageData;
     }
-    public float getPosHeight(int relativeX, int relativeY){
-        double R = Math.Sqrt(relativeX*relativeX + relativeY*relativeY);
-        double theta = Math.Atan2(relativeY, relativeX);
+    public float getPosHeight(int relativeX, int relativeZ){
+                
+        double R = Math.Sqrt(relativeX*relativeX + relativeZ*relativeZ);
+        double theta = Math.Atan2(relativeZ, relativeX);
 
         int newX = (int)(R*Math.Cos(theta+rotation));
         int newY = (int)(R*Math.Sin(theta+rotation));
@@ -30,6 +34,6 @@ public class MapElement
         if(Math.Abs(newX)*pixelWidth > halfSize || Math.Abs(newY)*pixelWidth > halfSize)
             return 0;
 
-        return uTool.LerpPosHeight(newX+halfSize, newY+halfSize, 0, pixelWidth, imageData);
+        return uTool.LerpPosHeight(newX+halfSize, newY+halfSize, 0, pixelWidth, imageData)*(float)(scale);
     }
 }
